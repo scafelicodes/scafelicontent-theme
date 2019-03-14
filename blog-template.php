9f22c58b2f8eeacd8818f744b2828c987bfbb82b
Gustavo Souza <39910511+gustavocfls@users.noreply.github.com>
2019-03-13 23:44:05 -0300

    indent

--- blog-template.php
+++ blog-template.php
@@ -8,65 +8,51 @@
     </div>
 </section>
 
-<div class="container">
-    
-  		<div class="row">
+<div class="container">    
+  	<div class="row">
+        <div class="col-md-9">
+            
+            <?php
+                $wp_query = new WP_Query();
+                query_posts( array( 'post_type' => 'post', 'showposts' => 6, 'paged'=>$paged ));
+                if(have_posts()):
+                while ($wp_query -> have_posts()) : $wp_query -> the_post();
+            ?>
 
-            <div class="col-md-9">
-
-
-                <?php
-                    $wp_query = new WP_Query();
-                    query_posts( array( 'post_type' => 'post', 'showposts' => 6, 'paged'=>$paged ));
-                    if(have_posts()):
-                    while ($wp_query -> have_posts()) : $wp_query -> the_post();
-                ?>
-
-                <div class="row post-preview">
-
-                    <div class="col-md-6">
-                        
-                        <a href="<?php the_permalink();?>"><?php the_post_thumbnail('singlelist'); ?></a>                    
-                    </div>
-
-                    <div class="col-md-6">
-                        <div class="desc-post">
-                            <p class="cat-preview"><?php
-                            $category = get_the_category(); ?>
-                            <?php echo $category[0]->cat_name; ?></p>
-                            <p class="date-preview"><?php the_time('j \d\e F \d\e Y') ?></p>    
-                            <a class="title-preview" href="<?php the_permalink();?>"><?php the_title(); ?></a>
-                        </div>
-                    </div>
+            <div class="row post-preview">
+                <div class="col-md-6">
                     
+                    <a href="<?php the_permalink();?>"><?php the_post_thumbnail('singlelist'); ?></a>                    
                 </div>
-
-                <?php endwhile; endif; ?>
-                
+                <div class="col-md-6">
+                    <div class="desc-post">
+                        <p class="cat-preview"><?php
+                        $category = get_the_category(); ?>
+                        <?php echo $category[0]->cat_name; ?></p>
+                        <p class="date-preview"><?php the_time('j \d\e F \d\e Y') ?></p>    
+                        <a class="title-preview" href="<?php the_permalink();?>"><?php the_title(); ?></a>
+                    </div>
+                </div>                
             </div>
+            <?php endwhile; endif; ?>            
+        </div>
 
-            <?php get_sidebar(); ?>
+        <?php get_sidebar(); ?>
 
-		</div>
-		
+    </div>		
 </div>
 
-
 <div class="container text-center">
-
-                <?php
-global $wp_query;
-
-$big = 999999999; // need an unlikely integer
-
-echo paginate_links( array(
-    'base' => str_replace( $big, '%#%', esc_url( get_pagenum_link( $big ) ) ),
-    'format' => '?paged=%#%',
-    'current' => max( 1, get_query_var('paged') ),
-    'total' => $wp_query->max_num_pages
-) );
-?>
-
+    <?php
+        global $wp_query;
+        $big = 999999999; // need an unlikely integer
+        echo paginate_links( array(
+            'base' => str_replace( $big, '%#%', esc_url( get_pagenum_link( $big ) ) ),
+            'format' => '?paged=%#%',
+            'current' => max( 1, get_query_var('paged') ),
+            'total' => $wp_query->max_num_pages
+        ) );
+    ?>
 </div>
 
 <?php get_footer(); ?>
