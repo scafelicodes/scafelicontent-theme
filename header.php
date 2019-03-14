585c0afd2c29e375c031b5e77ca1f83d6d7b31ff
Gustavo Souza <39910511+gustavocfls@users.noreply.github.com>
2019-03-13 23:48:05 -0300

    Update header.php

--- header.php
+++ header.php
@@ -4,17 +4,11 @@
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
-
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
-
-
     <link rel="stylesheet" type="text/css" href="<?php bloginfo('template_url') ?>/fonts/fonts.min.css" />
-
     <link rel="stylesheet" href="<?php bloginfo('template_url') ?>/style.css">
-
     <title><?php if(is_home()) { echo bloginfo("name"); echo " - "; echo bloginfo("description"); } else { echo wp_title(" - ", false, right); } ?></title>
   </head>
   <?php wp_head(); ?>
